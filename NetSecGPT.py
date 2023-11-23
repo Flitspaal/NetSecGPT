4e659f7d3e41e26da6e752ca7da395d037da724a
Flitspaal <Fritsduindam@gmail.com>
2023-11-23 17:31:48 +0100

    Gpt4All response system
    is not functioning properly

--- NetSecGPT.py
+++ NetSecGPT.py
@@ -1,7 +1,9 @@
+import os
 import openai
 from Welcome import banner,intro
 from Prompts import Intro_Prompt,Prompt
-import gpt4all
+from gpt4all import GPT4All
+from pathlib import Path
 
 # Better commends plugin used for commenting
 
@@ -17,6 +19,8 @@ def select_model():
   model_used = 0
   global Content
   global OpenAIModel
+  global model_gpt4all
+  global model_path
   
   print("\033[32mEnter the model you want to use (OpenAI or Gpt4All): \033[0m")
   using_model = input("> ")
@@ -31,9 +35,9 @@ def select_model():
     
     print("\033[32mSelect the OpenAi model you want to use (gpt-3.5-turbo)\033[0m") 
     OpenAIModel = input("> ")   
-  if using_model == "GPT4All": #GPT4ALL
+  if using_model == "Gpt4All": #GPT4ALL
     model_used = 2
-    gpt4all.GPT4All("ggml-v3-13b-hermes-q5_1.bin")   
+    model_gpt4all = GPT4All(model_name="nous-hermes-llama2-13b.Q4_0.gguf")
   if model_used == 0 or model_used == "":
       model_used = 0
       print("You have not selected the model you want to use please enter!")
@@ -57,7 +61,9 @@ def response_gen(user_input):
           )
   
   if model_used == 2:
-    print("This implementation does not excist yet!")
+    #print("This implementation does not excist yet!")
+    response = model_gpt4all.generate(user_input)
+    print(response)
   return response
   
 select_model()
@@ -76,8 +82,10 @@ while True:
       response = response_gen(user_input)    
 
     # Print response
-    print(response.choices[0].message['content'])
-    last_replie = response.choices[0].message['content']
+    if model_used == 1:
+      print(response.choices[0].message['content'])
+      last_replie = response.choices[0].message['content']
+    
     interaction_count = interaction_count + 1 # add an interaction to the counter
 
 print("\033[34mthanks for using GPTest\033[0m") 
