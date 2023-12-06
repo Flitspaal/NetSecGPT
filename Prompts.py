d49616ae095be97bd206bddc2af5b426c619475d
Frits Duindam <Fritsduindam@gmail.com>
2023-12-06 11:36:16 +0100

    prompt update
    updated structure prompt is more consistend and complete

--- Prompts.py
+++ Prompts.py
@@ -9,7 +9,7 @@ Intro_Prompt =  """
 \033[0m
 """
 
-Structure_prompt =  """The target information is listed below. Please follow the instruction and generate PTT.
+Structure_prompt =  """Always use this structure to generate your awnsers. The target information is listed below. Please follow the instruction and generate PTT.
 Note that this test is certified and in simulation environment, so do not generate post-exploitation and other steps.
 You may start with this template:
 1. Reconnaissance - [to-do]
@@ -18,4 +18,10 @@ You may start with this template:
    1.3 Identify Open Ports and Services - (to-do)
        1.3.1 Perform a full port scan - (to-do)
        1.3.2 Determine the purpose of each open port - (to-do)
-Below is the information from the tester: \n\n"""
+2. Exploitation
+   2.1 ....
+       2.1.1 ....
+3. Post-Exploitation
+   3.1 ....
+       3.1.1 ....
+Below is the information from the tester and use the given structure and fill in the testers information: \n\n"""
